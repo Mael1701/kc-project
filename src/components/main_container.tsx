13e9c00bd801aa69e99b40aadbf779f05b10fc9c
Mael Micout <gh api micoutmael3@gmail.com>
2023-03-01 23:23:16 +0100

    main container done

--- src/components/main_container.tsx
+++ src/components/main_container.tsx
@@ -10,14 +10,20 @@ function MainContainer() {
       <h1>Le Blue Wall</h1>
         <div className="container">
           <p>
-          Team Vitality est née en 2013 de la passion d’amis aux rêves les plus fous. Basés en plein coeur de Paris, nous sommes aujourd’hui l’un des clubs d’esport les plus influents et performants d’Europe, avec l’ambition d’étendre notre impact dans le monde entier.
-
-Nous évoluons sur les plus grandes licences d’esport : Counter-Strike, League of Legends, Rocket League, Valorant, FIFA, et nos équipes comptent plus de 100 titres nationaux et internationaux. Au-delà de la performance, nous travaillons pour préparer les générations futures et révéler tout leur talent.
+           La Karmine Corp (surnommée KCorp) est une structure française centrée autour d'une équipe e-sport immatriculée
+           à Tours et basée à Paris. Elle est formée le 30 mars 2020 sous le nom de Kameto Corp par le duo de streamers
+           francophones Kameto et Kotei. Le 16 novembre 2020, l'entrepreneur et rappeur français Prime rejoint la direction
+           de la structure, et l'équipe prend sa dénomination actuelle. Une société par actions simplifiée, présidée par
+           Kameto et dirigée par Prime, est créée quelques mois plus tard.<br/><br/>
+           L'équipe League of Legends nous a value le surnom de "Blue Wall" avec notre domination sur l'Europe au niveau
+           des ERLS pendant quasiment 2 ans de suite. Maintenant on nous connaît dans le monde entier, et nous representons nos couleurs sur 6 jeux différents
+           ( League of legends, Valorant, Rocket League, Teamfight Tactic, TrackMania et Super Smash Bros Ultimate). Nous avons
+           pour ambition de de devenir Champion du Monde sur chacun de ces jeux.
           </p>
               <img
                 className="d-block onekc"
                 src={image1.src}
-                alt="KC LoL;"
+                alt="KC"
               />
         </div>
     </div>
